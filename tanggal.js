e9e85dd91be711ab904951aac1f2ac8ef654f848
Surrendra <31328726+Surrendra@users.noreply.github.com>
2019-04-08 09:04:19 +0700

    tambahan set tanggal untuk modal

--- tanggal.js
+++ tanggal.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-	var kolom = $('.tanggal');
+    var kolom = $('.tanggal');
 	for (var i = 0; i < kolom.length; i++) {
 		var idvalue = kolom[i].id+'val'+i;
 		var triger = "makeTanggal('"+kolom[i].id+"','"+idvalue+"');";
-		$('#'+kolom[i].id).replaceWith('<div class="input-group"> <div class="input-group-addon" onclick="'+triger+'"><i class="fa fa-calendar"></i></div> <input type="text" class="form-control tanggal"  onclick="'+triger+'" id="'+kolom[i].id+'" value="'+convertDate(kolom[i].value)+'" required> <input type="hidden" value="'+kolom[i].value+'" name="'+kolom[i].id+'" id="'+idvalue+'"> </div>')
+		$('#'+kolom[i].id).replaceWith('<div class="input-group"> <div class="input-group-addon" onclick="'+triger+'"><i class="fa fa-calendar"></i></div> <input type="text" class="form-control tanggal" autocomplete="off"  onclick="'+triger+'" id="'+kolom[i].id+'" value="'+convertDate(kolom[i].value)+'" placeholder="dd/mm/yyyy"  required="required"> <input type="hidden" value="'+kolom[i].value+'" name="'+kolom[i].name+'" id="'+idvalue+'"> </div>');    
+           $('.tanggal').keydown(function(e){
+                e.preventDefault();
+            });
 	}
 });
+
 
 function convertDate(tgl) {
     if (tgl) {
@@ -15,9 +19,9 @@ function convertDate(tgl) {
         var tanggal_indo = tanggal[2]+' '+bulan[bln]+' '+tanggal[0];	
         return tanggal_indo;
     }
-    return 'dd/mm/yyyy';
+    return '';
 }
-
+    
 function makeTanggal(tgl,kvalue) {
     $('#'+tgl).datepicker({
         dateFormat:'dd MM yy',
@@ -25,7 +29,17 @@ function makeTanggal(tgl,kvalue) {
         altField:'#'+kvalue,
         changeMonth: true,
         changeYear: true,
+        yearRange: '1940:2040',
+        beforeShow: function() {
+            setTimeout(function(){
+                $('.ui-datepicker').css('z-index', 99999999999999);
+            }, 0);
+        },
     });
     $('#'+tgl).datepicker('show');
 }
 
+function setDate(cLabel,cValue,tanggal) {
+    $('#'+cLabel).val(convertDate(tanggal));
+    $('#'+cValue).val(tanggal);
+}
